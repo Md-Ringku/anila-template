4a9ee101681f7b9029311ffc746abe783c9f17d7
Md Rinku <79092161+Md-Ringku@users.noreply.github.com>
2021-06-12 13:13:17 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+ $("nav").toggleClass("scrolled",$(this).scrollTop()>100)
+});
+ $('.owl-skills').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay:1000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1140:{
+            items:1
+        }
+    }
+});  
+});
